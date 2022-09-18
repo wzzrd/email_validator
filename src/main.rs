57e2c15820cf2d42d18394145b72bbd9cc4d03d5
Maxim Burgerhout <maxim@wzzrd.com>
2022-09-18 15:32:17 +0200

    Reformatting & updating dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,26 @@
-#[macro_use] extern crate serde_derive;
+#[macro_use]
+extern crate serde_derive;
 
-use std::collections::{BTreeMap, HashMap};
-use std::fmt;
-use actix_web::{App, Error, HttpResponse, HttpServer};
-use actix_web::middleware::Logger;
-use paperclip::actix::{
-    OpenApiExt, Apiv2Schema, api_v2_operation,
-    web::{self, Json},
-};
-use paperclip::api_v2_schema;
-use check_if_email_exists::syntax::{check_syntax, SyntaxDetails};
-use serde::{Deserialize, Serialize};
-use gethostname::gethostname;
-use indexmap::IndexMap;
-use paperclip::v2::models::{Contact, DefaultApiRaw, Info, License};
-use log;
-use strum::Display;
 use actix_cors::Cors;
 use actix_web::http::header;
+use actix_web::middleware::Logger;
+use actix_web::{App, Error, HttpResponse, HttpServer};
+use check_if_email_exists::syntax::{check_syntax, SyntaxDetails};
+use gethostname::gethostname;
+use indexmap::IndexMap;
+use log;
 use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+use paperclip::actix::{
+    api_v2_operation,
+    web::{self, Json},
+    Apiv2Schema, OpenApiExt,
+};
+use paperclip::api_v2_schema;
+use paperclip::v2::models::{Contact, DefaultApiRaw, Info, License};
+use serde::{Deserialize, Serialize};
+use std::collections::{BTreeMap, HashMap};
+use std::fmt;
+use strum::Display;
 
 #[derive(Deserialize, Apiv2Schema)]
 /// The email address to be checked
@@ -50,7 +52,7 @@ impl From<SyntaxDetails> for VerifiedEmail {
             address,
             domain: s.domain.into(),
             is_valid_syntax: s.is_valid_syntax.into(),
-            username: s.username.into()
+            username: s.username.into(),
         }
     }
 }
@@ -60,7 +62,7 @@ impl From<SyntaxDetails> for VerifiedEmail {
 enum version_lifecycle {
     ACTIVE,
     DEPRECATED,
-    DRAFT
+    DRAFT,
 }
 
 /// Validate the email address
@@ -73,26 +75,46 @@ async fn validate_address(a: Json<Email>) -> Result<web::Json<VerifiedEmail>, Er
     Ok(Json(VerifiedEmail::from(res)))
 }
 
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "debug");
     env_logger::init();
-
-
 
     log::info!("Setting schema defaults");
     let mut spec = DefaultApiRaw::default();
     let badges = serde_json::json!([{"name": "env", "value": "dev"}, {"name": "security", "value": "medium"}, { "name": "region", "value": "global"}]);
     let mut info_exts = BTreeMap::new();
     info_exts.insert("x-category".to_string(), serde_json::json!("Utility APIs"));
-    info_exts.insert("x-long-description".to_string(), serde_json::Value::String(include_str!("../README.md").to_string()));
-    info_exts.insert("x-thumbnail".to_string(), serde_json::Value::String("https://en.gravatar.com/userimage/3149428/abb6f0635c488a6833a4966c9cff4ea2.jpeg".to_string()));
-    info_exts.insert("x-version-lifecycle".to_string(), serde_json::Value::String(version_lifecycle::ACTIVE.to_string()));
-    info_exts.insert("x-collections".to_string(), serde_json::Value::Array(vec![serde_json::Value::String("consumer-onboarding".to_string())]));
-    info_exts.insert("x-website".to_string(), serde_json::Value::String("https://www.wzzrd.com".to_string()));
+    info_exts.insert(
+        "x-long-description".to_string(),
+        serde_json::Value::String(include_str!("../README.md").to_string()),
+    );
+    info_exts.insert(
+        "x-thumbnail".to_string(),
+        serde_json::Value::String(
+            "https://en.gravatar.com/userimage/3149428/abb6f0635c488a6833a4966c9cff4ea2.jpeg"
+                .to_string(),
+        ),
+    );
+    info_exts.insert(
+        "x-version-lifecycle".to_string(),
+        serde_json::Value::String(version_lifecycle::ACTIVE.to_string()),
+    );
+    info_exts.insert(
+        "x-collections".to_string(),
+        serde_json::Value::Array(vec![serde_json::Value::String(
+            "consumer-onboarding".to_string(),
+        )]),
+    );
+    info_exts.insert(
+        "x-website".to_string(),
+        serde_json::Value::String("https://www.wzzrd.com".to_string()),
+    );
     info_exts.insert("x-public".to_string(), serde_json::Value::Bool(false));
-    info_exts.insert("termsOfService".to_string(), serde_json::Value::String("https:///www.wzzrd.com/tos".to_string()));
+    info_exts.insert(
+        "termsOfService".to_string(),
+        serde_json::Value::String("https:///www.wzzrd.com/tos".to_string()),
+    );
     info_exts.insert("x-badges".to_string(), badges);
 
     let mut root_exts = BTreeMap::new();
@@ -103,8 +125,15 @@ async fn main() -> std::io::Result<()> {
 
     spec.info = Info {
         version: "0.3.0".into(),
-        contact: Some(Contact { name: Some("Maxim Burgerhout".to_string()), email: Some("maxim@wzzrd.com".to_string()), url: Some("https://www.wzzrd.com".to_string()) }),
-        license: Some(License { name: Some("something legal".to_string()), url: Some("https://www.wzzrd.com".to_string()) }),
+        contact: Some(Contact {
+            name: Some("Maxim Burgerhout".to_string()),
+            email: Some("maxim@wzzrd.com".to_string()),
+            url: Some("https://www.wzzrd.com".to_string()),
+        }),
+        license: Some(License {
+            name: Some("something legal".to_string()),
+            url: Some("https://www.wzzrd.com".to_string()),
+        }),
         title: "Email address verification GitHub Test".into(),
         description: Some("This API verifies the validity of email addresses".to_string()),
         extensions: info_exts,
@@ -112,7 +141,9 @@ async fn main() -> std::io::Result<()> {
 
     log::info!("Starting up on {}", gethostname().into_string().unwrap());
     let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
-    builder.set_private_key_file("key.pem", SslFiletype::PEM).unwrap();
+    builder
+        .set_private_key_file("key.pem", SslFiletype::PEM)
+        .unwrap();
     builder.set_certificate_chain_file("cert.pem").unwrap();
     let port = std::env::var("PORT")
         .unwrap_or_else(|_| "8080".to_string())
@@ -126,9 +157,8 @@ async fn main() -> std::io::Result<()> {
             .wrap(Cors::permissive())
             .service(web::resource("/v1/validate").route(web::post().to(validate_address)))
             .build()
-        })
+    })
     .bind_openssl(("0.0.0.0", port), builder)?
     .run()
     .await
-    
 }
